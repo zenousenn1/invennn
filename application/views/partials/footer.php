3656c8a898ef8d57bb32cb4d92d5b4adfbcb20ba
zenousenn1 <renaizenousen@gmail.com>
2024-06-20 20:58:27 +0700

    RUQQ
    RRE

---
+++ application/views/partials/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+	<div class="container my-auto">
+		<div class="copyright text-center my-auto">
+			<span>aku gae dewe jhon tapi di ewangi haha <a href="https://github.com/zenousenn1" target="_blank">Faruq Adrean</a></span>
+		</div>
+	</div>
+</footer>
